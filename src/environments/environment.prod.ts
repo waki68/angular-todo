d27d41172f479f8dd08b42c8be1615968f36bb4e
waki68 <code_venturi@outlook.com>
2019-12-26 13:30:08 +0330

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA8ahapmt4IdrVlb2IMdrdsJ_LDgbeTglY",
+    authDomain: "irantalent-todo.firebaseapp.com",
+    databaseURL: "https://irantalent-todo.firebaseio.com",
+    projectId: "irantalent-todo",
+    storageBucket: "irantalent-todo.appspot.com",
+    messagingSenderId: "614839191380",
+    appId: "1:614839191380:web:487968436e99e6f8706ac1"
+  }
 };
